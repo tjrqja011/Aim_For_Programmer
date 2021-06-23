6c95eca8a8f7c67ec41df7c612e96049ca921c30
tjrqja011 <77607060+tjrqja011@users.noreply.github.com>
2021-06-23 20:30:35 +0900

    Add files via upload

---
+++ (1)HelloWorld/소스.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //라이브러리 추가
+
+void main()
+{
+	// cout : 출력명령어 추가
+	// <<   : 명령 구분
+	// endl : 줄바꿈 문자
+	// ;    : 명령이 끝났음을 알리는 문자
+	std::cout << "hello world" << std::endl;
+
+	system("pause");
+}
